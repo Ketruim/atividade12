58d1bb6abc2d8801fe432eac54c63b550ac2a5e2
Ketruim <ketruimgamer@gmail.com>
2025-02-17 19:14:17 -0300

    Update validators.dart

--- lib/widgets/lib/utils/validators.dart
+++ lib/widgets/lib/utils/validators.dart
@@ -1,9 +1,1 @@
-class Validators {
-  static String? validateName(String? value) {
-    if (value == null || value.isEmpty) {
-      return 'Nome não pode estar vazio';
-    }
-    return null;
-  }
-}
-```
+
